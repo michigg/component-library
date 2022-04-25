4912a1245f9871b68ea26a67b17694f137aab38a
Michael Götz <michael-guenther.goetz@stud.uni-bamberg.de>
2022-04-25 08:39:56 +0200

    chore: add jest and storybook

---
+++ src/atoms/Badge/Badge.spec.ts
@@ -0,0 +1,10 @@
+// TODO: implement tests
+
+describe('Badge.vue', () => {
+    describe(':props', ()=> {
+        it('Test Nothing', () => {})
+    })
+    describe('@events', ()=> {
+        it('Test Nothing', () => {})
+    })
+})
--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -1,6 +1,6 @@
 import Badge from './Badge/Badge.vue'
-import ButtonBase from './ButtonBase.vue'
+import ButtonBase from './Button/ButtonBase.vue'
 import Headline from './Headline.vue'
-import Icon from './Icon.vue'
+import Icon from './Icon/Icon.vue'
 
 export { Badge, ButtonBase, Headline, Icon }
