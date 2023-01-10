d5b4c471e82d385bf350d34568e13d366c5a9415
Michael Götz <mgoetz1995@gmail.com>
2023-01-10 09:07:56 +0100

    chore: add loading and disabled state

--- src/atoms/button/BaseButton/BaseButton.vue
+++ src/atoms/button/BaseButton/BaseButton.vue
@@ -1,18 +1,34 @@
 <template>
-  <button v-bind="$attrs" :class="[active ? 'active' : '']">
+  <button 
+    v-bind="$attrs" 
+    :class="{active, busy}"
+    :disabled="disabled || busy"
+  >
     <slot />
   </button>
 </template>
 <script lang="ts" setup>
 withDefaults(
-  defineProps<{ active?: boolean }>(),
-  { active: false }
+  defineProps<{ 
+    active?: boolean, 
+    busy?: boolean,
+    disabled: boolean
+  }>(),
+  { active: false, busy: false, disabled: false }
 )
 </script>
 <style scoped>
 button {
-  background-color: var(--color-surface-button);
-  color: var(--color-text-button);
+  --_button-color-surface: var(--button-color-surface, var(--color-surface-button));
+  --_button-color-surface-disabled: var(--button-color-surface-disabled, var(--color-primary-500-disabled));
+  --_button-color-loading-indicator: var(--button-color-loading-indicator, var(--color-primary-400));
+  --_button-color-text: var(--button-color-text, var(--color-text-button));
+}
+
+button {
+  position: relative;
+  background-color: var(--_button-color-surface);
+  color: var(--_button-color-text);
   padding: 0.5rem 1rem;
   border: none;
   border-radius: var(--radius-md);
@@ -22,9 +38,29 @@ button {
   transition: box-shadow 150ms ease-out;
 }
 
-button:hover,
-button:active,
-button.active {
+button:hover:not([disabled]),
+button:active:not([disabled]),
+button.active:not([disabled]) {
   box-shadow: var(--shadow-inset-md);
+}
+
+button[disabled] {
+  cursor: unset;
+  background-color: var(--_button-color-surface-disabled);
+}
+
+button.busy {
+  background-size: 200% 100%;
+  background-image: linear-gradient(90deg, var(--_button-color-surface-disabled) 40%, var(--_button-color-surface) 50%, var(--_button-color-surface-disabled) 60%);
+  animation: busyGradient 1.5s linear infinite;
+}
+
+@keyframes busyGradient {
+  0% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: -100% 50%;
+  }
 }
 </style>
